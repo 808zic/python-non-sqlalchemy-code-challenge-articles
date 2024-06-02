490477c4640d9d7920095f6856448cef19433704
Georgemugo <georgepmash@gmail.com>
2024-06-02 13:15:03 +0300

    Adds validation

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,61 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        self._title = title
+        Article.all.append(self)
+        if hasattr(author, '_articles'):
+            author._articles.append(self)
+        else:
+            author._articles = [self]
+        if hasattr(magazine, '_articles'):
+            magazine._articles.append(self)
+        else:
+            magazine._articles = [self]
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title is immutable")
+
+
 class Author:
     def __init__(self, name):
         self.name = name
+        self._articles = []
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
-        pass
+        Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        return list(set(article.magazine.category for article in self._articles))
+
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        return list(set(article.author for article in self._articles))
